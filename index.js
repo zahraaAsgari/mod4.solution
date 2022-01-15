6e968f969d74065c1be03dfed10c70b93d31d413
Zahra <zahraasgari345@gmail.com>
2022-01-15 22:39:56 +0330

    added

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 (function() {
-    var names = ["Zahra", "John", "Jack", "Jason", "Amin", "Sarah", "lara", "Jimy"];
+    var names = ["Yaakov", "John", "Jen", "Jason", "Paul", "Frank", "Larry", "Paula", "Laura", "Jim"];
 
     for (var i = 0; i < names.length; i++) {
         var firstLetter = names[i].charAt(0).toLocaleLowerCase();
